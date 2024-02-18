eeb45c056a5efd0ef0d95e96396a4c5bef990057
12joseca12 <sanchezgarciajosecarlos12@gmail.com>
2024-02-18 15:36:27 +0100

    revision-final
    enviado

--- js.js
+++ js.js
@@ -1,6 +1,3 @@
 document.addEventListener("DOMContentLoaded", function () {const navbar = document.querySelector(".navbar");
 window.addEventListener("scroll", function () {if (window.scrollY > 0) {navbar.classList.add("scrolled");} else {navbar.classList.remove("scrolled");}});});
 var swiper = new Swiper(".blog-slider", {spaceBetween: 30, effect: "fade", loop: true,mousewheel: {invert: false,}, pagination: {el: ".blog-slider__pagination", clickable: true,},});
-$(".submit").click(function () {return false;});
-$(document).ready(function () {$(".trigger").click(function () {$(".modal-wrapper").toggleClass("open");$(".page-wrapper").toggleClass("blur");return false;});});
-
